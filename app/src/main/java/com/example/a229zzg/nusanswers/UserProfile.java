f3d3a7a274fa9f7c1433c06eecc44c55162c9559
TravisC <travisching007@gmail.com>
2018-06-30 17:04:24 +0800

    Modified UserProfile activity :     Included image view and correct collapsing toolbar onOffsetChanged.

--- app/src/main/java/com/example/a229zzg/nusanswers/UserProfile.java
+++ app/src/main/java/com/example/a229zzg/nusanswers/UserProfile.java
@@ -2,11 +2,15 @@ package com.example.a229zzg.nusanswers;
 
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -14,11 +18,30 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.profile_toolbar);
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                ImageView userPicture = findViewById(R.id.user_picture);
+                Button button = findViewById(R.id.edit_profile_button);
+                if (Math.abs(verticalOffset)-appBarLayout.getTotalScrollRange() == 0)
+                {
+                    //  Collapsed
+                    button.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    //Expanded
+                    button.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
